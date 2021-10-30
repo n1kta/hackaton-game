179506a2e864ff12592e63d7ded96514eaf8820a
fantasseryoga <69531863+fantasseryoga@users.noreply.github.com>
2021-10-30 03:31:00 -0700

    animations_attack

--- src/anims/HeroAnims.ts
+++ src/anims/HeroAnims.ts
@@ -28,6 +28,41 @@ const CharacterAnims = (anims: Phaser.Animations.AnimationManager) => {
         frameRate: 10,
         repeat: -1
     });
+
+    anims.create({
+        key: 'hero_ult',
+        frames: anims.generateFrameNumbers('heroUlt', { start: 0, end: 9 }),
+        frameRate: 7,
+        repeat: -1
+    });
+
+    anims.create({
+        key: 'hero_down',
+        frames: anims.generateFrameNumbers('heroMooveUpDown', { start: 0, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    anims.create({
+        key: 'hero_up',
+        frames: anims.generateFrameNumbers('heroMooveUpDown', { start: 9, end: 17 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    anims.create({
+        key: 'hero_attack_right',
+        frames: anims.generateFrameNumbers('heroAttack', { start: 0, end: 4 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    anims.create({
+        key: 'hero_attack_left',
+        frames: anims.generateFrameNumbers('heroAttack', { start: 5, end: 7 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 export {
